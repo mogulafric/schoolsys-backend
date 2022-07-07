5852e815eaea87381ab23e37956ba0b319be32bc
leo-mutuku <leo-mutuku>
2022-07-07 19:51:51 +0300

    update

--- controller/students/students.js
+++ controller/students/students.js
@@ -13,9 +13,6 @@ const getAllStudents = catchAsync(async (req, res, next) => {
 const registerStudent = catchAsync(async (req, res, next) => {
   let {studentName, kcpeMarks, kcpeRank, pidNumber,yearOfAdmission,currentYear,gender,unitAdmission,unitCurrent, stream, id} = req.body;
   let admissionNumber =req.body.admissionNumber
-  console.log(unitAdmission)
-
-
   // check for duplicate student in the db
   const duplicate = await Student.findOne({
     admissionNumber: admissionNumber,
@@ -38,7 +35,7 @@ const registerStudent = catchAsync(async (req, res, next) => {
   res.status(201).json(result);
 });
 const updateStudent = catchAsync(async (req, res, next) => {
-  const {
+  let {
     studentName,
     admissionNumber,
     kcpeMarks,
@@ -53,30 +50,26 @@ const updateStudent = catchAsync(async (req, res, next) => {
     _id,
   } = req.body;
   if (!req?.body?._id) {
-    return res.status(400).json({ message: "ID parameter is required." });
+    return res.status(400).json({status:'failed', message: "ID parameter is required." });
   }
-
   const student = await Student.findOne({ _id: req.body._id }).exec();
   if (!student) {
     return res
-      .status(204)
-      .json({ message: `No student matches ID ${req.body._id}.` });
+      .status(400)
+      .json({status:'failed', message: `No student matches ID ${req.body._id}.` });
   }
-  if (req.body?.studentName) student.studentName = req.body.studentName;
-
-  if (req.body?.admissionNumber)
-    student.admissionNumber = req.body.admissionNumber;
-  if (req.body?.kcpeMarks) student.kcpeMarks = req.body.kcpeMarks;
-  if (req.body?.kcperank) student.kcperank = req.body.kcperank;
-  if (req.body?.pidNumber) student.pidNumber = req.body.pidNumber;
-  if (req.body?.stream) student.stream = req.body.stream;
-  if (req.body?.yearOfAdmission)
-    student.yearOfAdmission = req.body.yearOfAdmission;
-  if (req.body?.currentYear) student.currentYear = req.body.currentYear;
-  if (req.body?.unitAdmission) student.unitAdmission = req.body.unitAdmission;
-  if (req.body?.unitCurrent) student.unitCurrent = req.body.unitCurrent;
-  if (req.body?.gender) student.gender = req.body.gender;
-  if (req.body?.stream) student.stream = req.body.stream;
+  if (!req.body?.studentName) studentName = student.studentName;
+  if (!req.body?.admissionNumber)admissionNumber = student.admissionNumber;
+  if (!req.body?.kcpeMarks) kcpeMarks = student.kcpeMarks;
+  if (!req.body?.kcpeRank) kcpeRank= student.kcpeRank;
+  if (!req.body?.pidNumber) pidNumber= student.pidNumber;
+  if (!req.body?.stream)stream= student.stream 
+  if (!req.body?.yearOfAdmission) yearOfAdmission = student.yearOfAdmission;
+  if (!req.body?.currentYear) currentYear = student.currentYear;
+  if (!req.body?.unitAdmission) unitAdmission=student.unitAdmission 
+  if (!req.body?.unitCurrent)unitCurrent= student.unitCurrent 
+  if (!req.body?.gender)gender= student.gender 
+  if (!req.body?.stream) stream= student.stream 
 
   const result = await Student.updateOne(
     { _id: _id },
@@ -94,9 +87,16 @@ const updateStudent = catchAsync(async (req, res, next) => {
       stream: stream,
     }
   );
-  res.json(result);
+  res.status(200).json({status:'success',result:result.length, data:result});
 });
-const getStudentById = catchAsync(async (req, res, next) => {});
+const getStudentById = catchAsync(async (req, res, next) => {
+    let _id = req.body._id
+    if(!_id) return res.status(400).json({status:'failed',message:'Error, user id not found'})
+    const getStudent = await Student.findOne({_id:_id})
+    if(!getStudent){return res.status(400).json({status:'failed', message:'Error, student not found'})}
+    res.status(200).json({status:'success', result:getStudent.length, data:getStudent})
+
+});
 const archiveStudent = catchAsync(async (req, res, next) => {
   const { id } = req.body;
   if (!req?.body?.id)
