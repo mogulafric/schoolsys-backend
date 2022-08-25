7891df7e6ef134b4fe9e381b99a7f072ec7f9af2
mogulafric <info@mogulafric.com>
2022-08-25 23:35:32 +0300

    q

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,9 +1,6 @@
 const allowedOrigins = [
     'http://127.0.0.1:5000',
-    'http://localhost:3500',
-    'http://localhost:3000',
-    'http://localhost:5000',
-    'http://localhost:3001',
+    'http://178.79.150.198',
     'http://localhost:4200',
     'http://192.168.0.104:4200'
 ];
